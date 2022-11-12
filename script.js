3730106eaf7144e21263189224edb29f99b077d9
Odi000 <mandimandi.ro@gmail.com>
2022-11-12 12:31:20 +0100

    Creating Calculator calculation Object

--- script.js
+++ script.js
@@ -5,18 +5,30 @@ const buttons = [...document.querySelectorAll('button')];
 
 //Input
 buttons.forEach(button => {
-    const btnValue = button.textContent;
-    if(btnValue === 'C' || btnValue === '=') return;
-    
-    button.addEventListener('click', () => {
-        const operator = /[]/;
-        const expContent = expression.textContent;
+	const btnValue = button.textContent;
+	if (btnValue === 'C' || btnValue === '=') return;
 
-        if(expression.clientWidth > 290) return;
-        if(/[²√]/.test(button.textContent) && expContent[expContent.length-1] === '√') return;
-        if(button.id === '²' && expContent[expContent.length-1] === '²') return;
-        if(btnValue === 'x²') {
-            return expression.textContent += button.id;
-        } expression.textContent += btnValue;
-    })
+	button.addEventListener('click', () => {
+		const operator = /[]/;
+		const expContent = expression.textContent;
+
+		if (expression.clientWidth > 290) return;
+		if (/[²√]/.test(button.textContent) && expContent[expContent.length - 1] === '√') return;
+		if (button.id === '²' && expContent[expContent.length - 1] === '²') return;
+		if (btnValue === 'x²') {
+			return expression.textContent += button.id;
+		} expression.textContent += btnValue;
+	})
 })
+
+//Calculator Engine Object
+const engine = {
+	'C': function(){},
+	'²': function(){},
+	'√': function(){},
+	'/': function(){},
+	'*': function(){},
+	'-': function(){},
+	'+': function(){},
+	'=': function(){},
+}
