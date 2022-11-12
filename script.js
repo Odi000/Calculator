c31230e7e06f9f14f469403cf509466fc18f59d9
Odi000 <mandimandi.ro@gmail.com>
2022-11-12 15:51:18 +0100

    Evaluated exp work a little bit

--- script.js
+++ script.js
@@ -5,30 +5,56 @@ const buttons = [...document.querySelectorAll('button')];
 
 //Input
 buttons.forEach(button => {
-	const btnValue = button.textContent;
-	if (btnValue === 'C' || btnValue === '=') return;
-
 	button.addEventListener('click', () => {
-		const operator = /[]/;
+		const btnValue = button.textContent;
+		const actionBtn = /[C/*\-+=]/;
 		const expContent = expression.textContent;
-
+		const onlyZero = /^0$/;
+		const integer = /\d/;
+		//Input rules
+		if (actionBtn.test(btnValue)) return engine[btnValue](btnValue);
 		if (expression.clientWidth > 290) return;
+		if (onlyZero.test(expContent) && integer.test(btnValue)) expression.textContent = '';
+		if (btnValue === '.' && expContent.includes('.')) return;
 		if (/[²√]/.test(button.textContent) && expContent[expContent.length - 1] === '√') return;
 		if (button.id === '²' && expContent[expContent.length - 1] === '²') return;
-		if (btnValue === 'x²') {
-			return expression.textContent += button.id;
-		} expression.textContent += btnValue;
+		if (btnValue === 'x²') return expression.textContent += button.id;
+		expression.textContent += btnValue;
 	})
 })
 
-//Calculator Engine Object
+//Calculator Calculation Engine Object
 const engine = {
-	'C': function(){},
+	'C': function(){
+		expression.textContent = '0';
+		evaluatedExp.textContent = '';	
+	},
 	'²': function(){},
 	'√': function(){},
-	'/': function(){},
+	'/': function(){
+		if (!evaluatedExp.textContent){
+			evaluatedExp.textContent = expression.textContent + '/';
+			expression.textContent = '0';
+		}
+	},
 	'*': function(){},
 	'-': function(){},
 	'+': function(){},
-	'=': function(){},
+	'=': function(){
+		const allOprtrRegEx = /[/*\-+²√]/;
+		const oprtrRegEx = /[/*\-+]/;
+		const evalExpContent = evaluatedExp.textContent;
+		let isNegative = false;
+
+		if (evalExpContent.textContent[0]){
+			isNegative = true;
+		}
+
+		if(!allOprtrRegEx.test(evalExpContent)) return;
+
+		evaluatedExp.textContent += expression.textContent ;
+		const operands = evaluatedExp.textContent.split(oprtrRegEx);
+
+		console.log(operands)
+	},
 }
